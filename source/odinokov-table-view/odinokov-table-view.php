c735a9f1f5c9b6cc2c96ebb10177e1161db9777e
Kirill Odinokov <odinokov@yandex.ru>
2026-08-14 11:37:55 +0300

    odinokov-table-view v1.0.4 - fix enqueue timing

--- source/odinokov-table-view/odinokov-table-view.php
+++ source/odinokov-table-view/odinokov-table-view.php
@@ -3,7 +3,7 @@
  * Plugin Name: Odinokov Table View
  * Plugin URI:  https://github.com/KirillOdinokov/wp-plugins
  * Description: Автоматический табличный вид для категорий WooCommerce с однотипными товарами. Управление выводом подкатегорий/товаров. Совместим с Porto.
- * Version:     1.0.3
+ * Version:     1.0.4
  * Author:      Odinokov
  * Author URI:  https://github.com/KirillOdinokov/wp-plugins
  * Text Domain: odinokov-table-view
@@ -11,7 +11,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'OTV_VERSION', '1.0.3' );
+define( 'OTV_VERSION', '1.0.4' );
 define( 'OTV_DIR', plugin_dir_path( __FILE__ ) );
 define( 'OTV_URL', plugin_dir_url( __FILE__ ) );
 
@@ -47,6 +47,7 @@ class Odinokov_Table_View {
         add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_assets' ] );
         add_filter( 'get_product_cat_metadata', [ $this, 'override_view_mode_meta' ], 20, 4 );
         add_filter( 'get_woocommerce_term_metadata', [ $this, 'override_display_type_meta' ], 20, 4 );
+        add_filter( 'body_class', [ $this, 'body_class' ] );
     }
 
     public function add_admin_menu() {
@@ -110,13 +111,6 @@ class Odinokov_Table_View {
 
     public function enqueue_assets() {
         if ( ! is_product_category() ) return;
-
-        $term = get_queried_object();
-        if ( ! $term || ! isset( $term->term_id ) ) return;
-
-        $cached = get_transient( 'otv_check_' . $term->term_id );
-        if ( false === $cached || empty( $cached['table'] ) ) return;
-
         wp_enqueue_style( 'otv-table', OTV_URL . 'assets/css/table.css', [], OTV_VERSION );
         wp_enqueue_script( 'otv-table', OTV_URL . 'assets/js/table.js', [], OTV_VERSION, true );
     }
@@ -252,11 +246,6 @@ class Odinokov_Table_View {
         $ratio = $max_count / $total;
 
         return $ratio >= 0.8;
-    }
-
-    public function table_header() {
-        if ( ! $this->is_table_view ) return;
-        echo '<div class="otv-table-header"><div class="otv-th otv-th-img">' . esc_html__( 'Фото', 'odinokov-table-view' ) . '</div><div class="otv-th otv-th-name">' . esc_html__( 'Наименование', 'odinokov-table-view' ) . '</div><div class="otv-th otv-th-price">' . esc_html__( 'Цена', 'odinokov-table-view' ) . '</div><div class="otv-th otv-th-cart">' . esc_html__( 'В корзину', 'odinokov-table-view' ) . '</div></div>';
     }
 }
 
